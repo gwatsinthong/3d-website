9a58041231d89fd58855a0decc7add5b9cec863f
gwatsin <gwatsinthong@gmail.com>
2025-05-19 15:55:35 +0530

    half done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import LogoSection from "./components/LogoSection"
+import NavBar from "./components/NavBar"
 import Hero from "./sections/Hero"
+import ShowcaseSection from "./sections/ShowcaseSection"
 
 const App = () => {
   return (
-    <>
+    <>  
+        <NavBar />
         <Hero />
+        <ShowcaseSection />
+        <LogoSection />
     </>
   )
 }
